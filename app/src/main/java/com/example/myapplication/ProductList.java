c42439a1cc1cbfd1d1637a82a752169969a853eb
Arseniy Paramonov <paramosha228@gmail.com>
2025-04-13 17:16:28 +0500

    Commit

--- app/src/main/java/com/example/myapplication/ProductList.java
+++ app/src/main/java/com/example/myapplication/ProductList.java
@@ -6,13 +6,21 @@ import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class ProductList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.product_list);
+        setContentView(R.layout.activity_product_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 
     public void goBack(View v) {
